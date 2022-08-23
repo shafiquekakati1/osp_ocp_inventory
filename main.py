978b71983ec84600877e0aee2f895f471e009cab
Shafique Kakati <shafique.kakati@vodafone.om>
2022-08-23 20:51:04 +0400

    Added migration script

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 
+import sys
+sys.path.append("/home/cloud-user/.local/lib/python3.9/site-packages")
+import subprocess
+
+
 def login_ocp_prod():
     servername="https://api.rhocp.prod-cloud1.itcloud.local.vodafone.om:6443"
-    print(servername)
+    commands = [ "oc login -u kshitij.tyagi -p '\''RBJLFG@u4Z'\'' --server=https://api.rhocp.prod-cloud1.itcloud.local.vodafone.om:6443" ]
+    for x in range(len(commands)):
+        r = subprocess.Popen(commands[x], shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+        print(r)
 
 if __name__ == '__main__':
     login_ocp_prod()
